40187d2d29dc2351dbf302df597c4ed8647e480f
Juliano <Juliano@Munholi_Note>
2018-02-04 23:11:37 -0200

    thread sleep added to catch better the point

--- ecommerce/src/test/java/selenium/ecommerce/tests/BuyNewItem.java
+++ ecommerce/src/test/java/selenium/ecommerce/tests/BuyNewItem.java
@@ -1,6 +1,8 @@
 package selenium.ecommerce.tests;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import selenium.ecommerce.commons.Base;
 
@@ -14,7 +16,7 @@ import selenium.ecommerce.commons.Base;
  * fill all fields and finish action
  * close browser
  */
-
+@FixMethodOrder(MethodSorters.JVM)
 public class BuyNewItem extends Base {
 
 	String item = "Inspiron I11-3168-A10";
@@ -22,11 +24,18 @@ public class BuyNewItem extends Base {
 	int quantity = 1;
 	String email = null;
 	String pass = null;
-	
+
 	@Test
 	public void buyItem() throws InterruptedException {
-		
+
 		buyNewItem.login(email, pass);
 		buyNewItem.buyItem(item, quantity, paymentType);
+	}
+
+	@Test
+	public void buyMoreThanOneItem() throws InterruptedException {
+
+		buyNewItem.login(email, pass);
+		buyNewItem.buyItem(item, 2, paymentType);
 	}
 }
